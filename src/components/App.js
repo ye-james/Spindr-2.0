5b520d82dacf7cdf95f2c42048744e9f4c2a7fd1
James Ye <james101994@yahoo.com>
2023-04-26 10:14:19 -0700

    rework styling

--- src/components/App.js
+++ src/components/App.js
@@ -1,39 +1,27 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState } from "react";
 import { Routes, Route } from "react-router-dom";
-import MainPage from './mainPage';
-import Login from './login';
-
-
+import MainPage from "./mainPage";
+import Login from "./login";
 
 function App() {
-
-
-
   // A state that represents if user is logged in
   const [loggedIn, setLoggedIn] = useState(false);
   // Function that changes login status
   const handleLoginClick = (e) => {
     e.preventDefault();
     setLoggedIn(!loggedIn);
-  }
+  };
   // If user is logged in, return mainpage component
   return (
-    <div>
-
-      <Routes>
-        <Route exact path='/' element={<Login/>}/>
-        <Route path='/home' element={<MainPage/>}/>
-      </Routes>
-
-    </div>
-
-
-  )  
+    <Routes>
+      <Route exact path="/" element={<Login />} />
+      <Route path="/home" element={<MainPage />} />
+    </Routes>
+  );
   // if (loggedIn) {
   //    return <MainPage />
   //   }
-  // return <Login handleLoginClick={handleLoginClick}/>  
+  // return <Login handleLoginClick={handleLoginClick}/>
 }
 
 export default App;
-
