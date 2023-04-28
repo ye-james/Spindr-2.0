d4c4e7c7f00e3209f5c11df96cfe4a8db6ceb403
James Ye <james101994@yahoo.com>
2023-04-27 17:45:38 -0700

    update playlist to try to add to spotify

--- src/components/playlist.js
+++ src/components/playlist.js
@@ -1,9 +1,11 @@
 import React, { useState } from "react";
 import axios from "axios";
-import { useCookies } from "react-cookie";
+import { useCookies } from 'react-cookie';
+
 
 export default function Playlist({ playlist, setPlaylist }) {
-  const [cookies] = useCookies(["access_token"]);
+
+  const [cookies] = useCookies(['access_token']);
 
   console.log(playlist);
   const removeSong = (song) => {
@@ -32,31 +34,60 @@ export default function Playlist({ playlist, setPlaylist }) {
   const addToSpotify = () => {
     // const token = cookies.get('access_token');
     // Get value of specific cookie
-    const token = cookies["access_token"];
+    const token = cookies['access_token'];
 
-    // const tokenType = cookies.get('token_type');
-
-    fetch("https://api.spotify.com/v1/users/dom.c13/playlists", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-        Authorization: "Bearer " + token,
-      },
-      body: JSON.stringify({
-        name: "Spindr Playlist",
-        public: false, // set to true if you want the playlist to be public
-      }),
+    const trackUris = playlist.map(track => {
+      return track.trackUri
     })
-      .then((response) => response.json())
-      .then((data) => console.log("New playlist created:", data))
-      .catch((error) => console.error("Error creating playlist:", error));
 
-    console.log("current playlist: ", playlist);
-    const updatedPlaylist = playlist.filter((s) => {
-      s.trackUri;
-    });
-    console.log("updated playlist: ", playlist);
-  };
+    console.log(trackUris)
+    
+    // const tokenType = cookies.get('token_type');
+    
+    // fetch('https://api.spotify.com/v1/users/dom.c13/playlists', {
+    //   method: 'POST',
+    //   headers: {
+    //     'Content-Type': 'application/json',
+    //     'Authorization': 'Bearer ' + token
+    //   },
+    //   body: JSON.stringify({
+    //     'name': 'Spindr Playlist',
+    //     'public': false // set to true if you want the playlist to be public
+    //   })
+    // })
+    // .then(response => response.json())
+    // .then(data => {
+    //   console.log('New playlist created:', data);
+    //   const trackUris = playlist.map(track => {
+    //     return track.trackUri
+    //   })
+
+    //   console.log(trackUris)
+    //   const updatedPlaylist = playlist.filter((s) => {
+    //     return s.trackUri !== undefined;
+    //   })
+    //   fetch(`https://api.spotify.com/v1/playlists/${data.id}/tracks`, {
+    //     method: "POST",
+    //     headers: {
+    //       "Content-Type": "application/json",
+    //       Authorization: `Bearer ${token}`,
+    //     },
+    //     body: JSON.stringify({
+    //       uris: [trackUris],
+    //     })
+    //   }).then(response => response.json()).then(data => console.log(data))
+      
+    // })
+
+    // .catch(error => console.error('Error creating playlist:', error));
+
+    //console.log('current playlist: ', playlist);
+    // const updatedPlaylist = playlist.filter((s) => {
+    //     return s.trackUri !== undefined;
+    //   })
+    //console.log('updated playlist: ', updatedPlaylist);
+
+  }
 
   return (
     <div className="playlistContainer">
@@ -82,9 +113,7 @@ export default function Playlist({ playlist, setPlaylist }) {
           )}
         </div>
       </ul>
-      <button className="playlistBtn" onClick={addToSpotify}>
-        Create Playlist on Spotify
-      </button>
+      <button className="playlistBtn" onClick={addToSpotify}>Create Playlist on Spotify</button>
     </div>
   );
 }
